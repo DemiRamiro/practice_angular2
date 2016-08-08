27834870ba389d3da27187c04231c7b00b0ce46b
DemiRamiro <johndemi.ramiro@gmail.com>
2016-08-08 14:03:59 +0800

    Added Routing

---
+++ app/mock-heroes.ts
@@ -0,0 +1,9 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Alien Queen' },
+    { id: 12, name: 'Deku' },
+    { id: 13, name: 'One-punch man' },
+    { id: 14, name: 'Caped baldy' },
+    { id: 15, name: 'Uravity' }
+];
